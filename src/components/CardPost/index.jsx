5839ccf283de2390f2db953435887101b6c53b31
Igor Roberto <roberto.igor.18@gmail.com>
2025-07-20 02:53:17 -0300

    Criacao do componente card e sua estilização utilizando dados estaticos

---
+++ src/components/CardPost/index.jsx
@@ -0,0 +1,31 @@
+import Image from "next/image"
+import { Avatar } from "../Avatar"
+import styles from "./cardpost.module.css"
+
+export const CardPost = ({ post }) => {
+    return (
+        <article className={styles.card}>
+            <header className={styles.header}>
+                <figure className={styles.figure}>
+                    <Image
+                        src={post.cover}
+                        alt={`Capa do post de titulo: ${post.title}`}
+                        width={438}
+                        height={133}
+                    />
+                </figure>
+            </header>
+            <section className={styles.content}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
+                <a href="">Ver Detalhes</a>
+            </section>
+            <footer className={styles.footer}>
+                <Avatar
+                    imageSrc={post.author.avatar}
+                    name={post.author.username}
+                />
+            </footer>
+        </article>
+    )
+}
