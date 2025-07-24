515d77c0d65b8b64ca40b0bbe8ba22db8d005938
Igor Roberto <roberto.igor.18@gmail.com>
2025-07-24 04:07:48 -0300

    criacao de pagina para cada post usando o roteador do next js

--- src/components/CardPost/index.jsx
+++ src/components/CardPost/index.jsx
@@ -1,31 +1,34 @@
 import Image from "next/image"
 import { Avatar } from "../Avatar"
 import styles from "./cardpost.module.css"
+import Link from "next/link"
 
 export const CardPost = ({ post }) => {
     return (
-        <article className={styles.card}>
-            <header className={styles.header}>
-                <figure className={styles.figure}>
-                    <Image
-                        src={post.cover}
-                        alt={`Capa do post de titulo: ${post.title}`}
-                        width={438}
-                        height={133}
+        <Link className={styles.link} href={`/posts/${post.slug}`}>
+            <article className={styles.card}>
+                <header className={styles.header}>
+                    <figure className={styles.figure}>
+                        <Image
+                            src={post.cover}
+                            alt={`Capa do post de titulo: ${post.title}`}
+                            width={438}
+                            height={133}
+                        />
+                    </figure>
+                </header>
+                <section className={styles.content}>
+                    <h2>{post.title}</h2>
+                    <p>{post.body}</p>
+                    <a href="">Ver Detalhes</a>
+                </section>
+                <footer className={styles.footer}>
+                    <Avatar
+                        imageSrc={post.author.avatar}
+                        name={post.author.username}
                     />
-                </figure>
-            </header>
-            <section className={styles.content}>
-                <h2>{post.title}</h2>
-                <p>{post.body}</p>
-                <a href="">Ver Detalhes</a>
-            </section>
-            <footer className={styles.footer}>
-                <Avatar
-                    imageSrc={post.author.avatar}
-                    name={post.author.username}
-                />
-            </footer>
-        </article>
+                </footer>
+            </article>
+        </Link>
     )
 }
